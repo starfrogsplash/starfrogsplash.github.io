5809e83946d200b6ae31b508cf29ff611cd662a8
Ali Khalil <ali_khalil@hotmail.co.uk>
2019-05-31 16:29:26 +0100

    added in header animations

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import '../sass/main.css';
+import React , {Fragment}from "react";
+import "../sass/main.css";
+import LightSpeed from 'react-reveal/LightSpeed';
+import VisibilitySensor from "react-visibility-sensor";
 
 const repeat = (item, num) => {
-    const fireflys = [];
-    for (let i = 0; i < num; i++){
-       fireflys.push(item)
-    }
-    return fireflys
-}
+  const fireflys = [];
+  for (let i = 0; i < num; i++) {
+    fireflys.push(item);
+  }
+  return fireflys;
+};
 
 export const Header = () => (
-    <section id="home" className="header">
-    <div className="header__text">
-      <h1 className="header__text__name">Ali Khalil</h1>
-      <h1 className="header__text__title">Full Stack Web Developer</h1>
-    </div>
-    <div className="fireflySet">
+  <section  className="header">
+    <div id="home" className="header__text">
 
-    { repeat(<div className="firefly"></div>, 25)}
+        <VisibilitySensor>
+            {({ isVisible }) => (
+              <Fragment>
+                <LightSpeed left when={isVisible} duration={2000}>
+                <h1 className="header__text__name">Ali Khalil</h1>
+                </LightSpeed>
+                <LightSpeed right when={isVisible} duration={2000}>
+                <h1 className="header__text__title">Full Stack Web Developer</h1>
+                </LightSpeed>
+              </Fragment>
+            )}
+      </VisibilitySensor>
+
+
+
+
 
     </div>
-    <div id="about" className="aboutClipTop"></div>
+    <div className="fireflySet">{repeat(<div className="firefly" />, 25)}</div>
+    <div id="about" className="aboutClipTop" />
   </section>
-)
+);
--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ const NavBar = () => (
        <li className="nav__item"><AnchorLink className="nav__link" href='#home'>home</AnchorLink></li>
         <li className="nav__item"><AnchorLink className="nav__link" href='#About'>About</AnchorLink></li>
         <li className="nav__item"><AnchorLink className="nav__link" href='#Project'>home</AnchorLink></li>
-        <li className="nav__item"><a href="d" className="nav__link">Blog</a></li>
+        <li className="nav__item"><a href="#d" className="nav__link">Blog</a></li>
       </ul>
     </section>
 )
