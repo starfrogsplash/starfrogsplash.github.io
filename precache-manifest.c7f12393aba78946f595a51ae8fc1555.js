55225185ec13d0b0d135cd0fc1fe0da1361dabaf
Ali Khalil <ali_khalil@hotmail.co.uk>
2019-06-09 22:10:59 +0100

    Updates

---
+++ precache-manifest.c7f12393aba78946f595a51ae8fc1555.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8a7a5205ac0b764c39a608003463c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b5de1c1a56049f6455",
+    "url": "/static/css/main.c4b45d39.chunk.css"
+  },
+  {
+    "revision": "bfefed7c3c677616a188",
+    "url": "/static/js/2.0a15a8ae.chunk.js"
+  },
+  {
+    "revision": "41b5de1c1a56049f6455",
+    "url": "/static/js/main.6ed005fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77dcef68077ff5c5337ca3216007353b",
+    "url": "/static/media/2018-Cover.77dcef68.jpg"
+  },
+  {
+    "revision": "710ca2949c2a127422ea76ea2762b81c",
+    "url": "/static/media/AWS_combined.710ca294.png"
+  },
+  {
+    "revision": "fd34af01dc025fee5b0be55f0e4e9a22",
+    "url": "/static/media/Prince2.fd34af01.png"
+  },
+  {
+    "revision": "dac3bdb2b4426086130c4fdbcea5959c",
+    "url": "/static/media/Savoye LET Plain1.0.dac3bdb2.woff"
+  },
+  {
+    "revision": "feff963fc8b3ade0dc1d8413a44a476b",
+    "url": "/static/media/SignPainter HouseScript Regular.feff963f.ttf"
+  },
+  {
+    "revision": "0f34d9d23b046761ebbd7d3010c1e1e8",
+    "url": "/static/media/SignPainter-HouseBrush.0f34d9d2.woff"
+  },
+  {
+    "revision": "99ed8e0c01d5b390ebb81f082a0bd11f",
+    "url": "/static/media/Snell Roundhand Script.99ed8e0c.ttf"
+  },
+  {
+    "revision": "4ae6e2df0715cad9a893fedb02821c55",
+    "url": "/static/media/SnellBT-Regular.4ae6e2df.woff"
+  },
+  {
+    "revision": "f948790edc57bdb02a947bd44ec07c90",
+    "url": "/static/media/coming-soon.f948790e.png"
+  }
+]);
